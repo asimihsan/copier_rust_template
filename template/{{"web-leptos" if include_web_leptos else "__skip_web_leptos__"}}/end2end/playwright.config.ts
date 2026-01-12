e3af1830545ad3ee18899de9dbea623e46a0dc7d
Asim Ihsan <657831+asimihsan@users.noreply.github.com>
2026-01-11 19:59:38 -0500

    Switch web tooling to bun and update CI caches (#4)

--- template/{{"web-leptos" if include_web_leptos else "__skip_web_leptos__"}}/end2end/playwright.config.ts
+++ template/{{"web-leptos" if include_web_leptos else "__skip_web_leptos__"}}/end2end/playwright.config.ts
@@ -108,7 +108,7 @@ const config: PlaywrightTestConfig = {
 
   /* Run your local dev server before starting the tests */
   // webServer: {
-  //   command: 'npm run start',
+  //   command: 'bun run start',
   //   port: 3000,
   // },
 };
